c0ef80dd91377d246ddbae11ce696ee8e826ae50
orazi-paolo <orazi.paolo10@gmail.com>
2024-08-20 15:12:29 +0200

    ok mileston 1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* 
+1. MILESTONE 1
+Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider; avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
+
+2. MILESTONE 2
+Adesso rimuoviamo tutto il markup statico e inseriamo le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for. Possiamo concatenare una stringa con un template literal oppure utilizzare gli altri metodi di manipolazione del DOM che abbiamo visto insieme. Al termine di questa fase ci ritroveremo con lo stesso slider, ma costruito dinamicamente attraverso JavaScript.
+
+3. MILESTONE 3
+Al click dell'utente sulle frecce, l'immagine attiva cambia e diventa visibile nello slider, prendendo il posto della precedente.
+*/
